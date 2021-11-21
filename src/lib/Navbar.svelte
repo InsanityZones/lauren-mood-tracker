c59d22f0ea3c6f987d9a0b335508b6f35cba90d1
Lauren Chua <laurenchuaweiqian2008@gmail.com>
2021-11-21 07:25:39 +0000

    ok then

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="mood">Mood Tracker</a>
     </div>
   </nav>
